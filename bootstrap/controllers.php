43d3165d7e1733ab404e48664536654b71132e30
Alexis Wurth <alexis.wurth57@gmail.com>
2016-12-16 11:19:08 +0100

    Removed AuthController from container

--- bootstrap/controllers.php
+++ bootstrap/controllers.php
@@ -3,7 +3,3 @@
 $container['AppController'] = function ($container) {
     return new App\Controller\AppController($container);
 };
-
-$container['AuthController'] = function ($container) {
-    return new App\Controller\AuthController($container);
-};
