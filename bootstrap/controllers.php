021198920ddbc1f9f463e6d9844b8c9605234334
Alexis Wurth <alexis.wurth57@gmail.com>
2016-12-23 14:15:19 +0100

    Get, add, edit, delete countries

--- bootstrap/controllers.php
+++ bootstrap/controllers.php
@@ -4,6 +4,14 @@ $container['AppController'] = function ($container) {
     return new App\Controller\AppController($container);
 };
 
+$container['AdminController'] = function ($container) {
+    return new App\Controller\AdminController($container);
+};
+
+$container['CountryController'] = function ($container) {
+    return new App\Controller\CountryController($container);
+};
+
 $container['PointController'] = function ($container) {
     return new App\Controller\PointController($container);
 };
--- src/App/Resources/routes/app.php
+++ src/App/Resources/routes/app.php
@@ -3,3 +3,21 @@
 $app->get('/', 'AppController:home')->setName('home');
 
 $app->map(['GET', 'POST'], '/points/add', 'PointController:add')->setName('add_point');
+
+$app->group('/admin', function () {
+    $this->get('', 'AdminController:home')->setName('admin');
+
+    $this->group('/countries', function () {
+        $this->get('', 'CountryController:get')->setName('get_countries');
+        $this->map(['GET', 'POST'], '/add', 'CountryController:add')->setName('add_country');
+        $this->map(['GET', 'POST'], '/{id}/edit', 'CountryController:edit')->setName('edit_country');
+        $this->get('/{id}/delete', 'CountryController:delete')->setName('delete_country');
+    });
+
+    /*$this->group('/cities', function () {
+        $this->get('', 'CityController:get')->setName('get_countries');
+        $this->map(['GET', 'POST'], '/add', 'CityController:add')->setName('add_country');
+        $this->map(['GET', 'POST'], '/{name}/edit', 'CityController:edit')->setName('edit_country');
+        $this->get('/{name}/delete', 'CityController:delete')->setName('delete_country');
+    });*/
+});
