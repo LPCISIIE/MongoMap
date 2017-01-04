e4ba201d44a03a72345cac3c499ae19c0aae7847
xchopin <xchopin54@gmail.com>
2017-01-04 18:51:32 +0100

    Created Comment Controller

--- src/App/Controller/CommentController.php
+++ src/App/Controller/CommentController.php
@@ -1,15 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: xavier
- * Date: 04/01/17
- * Time: 18:05
- */
 
 namespace App\Controller;
 
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Respect\Validation\Validator as V;
 
 class CommentController
 {
+
+    public function API_get()
+    {
+        return json_encode($this->mongo->findById('Comment', $id));
+    }
 
 }
