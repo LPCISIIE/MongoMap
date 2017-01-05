2f1450e1282f7f9f6c85a9d410cde021a05a01d9
xchopin <xchopin54@gmail.com>
2017-01-05 19:02:36 +0100

    User can insert comments on locations

--- src/App/Controller/CommentController.php
+++ src/App/Controller/CommentController.php
@@ -9,28 +9,32 @@ use Respect\Validation\Validator as V;
 class CommentController extends Controller
 {
 
-    public function API_get(Request $request, Response $response, Array $arg)
+
+    public function API_get(Request $request, Response $response, $id)
     {
-        return json_encode($this->mongo->findById('Comment', $arg['id']));
+        return json_encode($this->mongo->where('comment', ['location_id' => $id])->toArray());
     }
 
     public function add(Request $request, Response $response)
     {
          $this->validator->validate($request, [
              'comment' => V::notBlank(),
-             'author' => V::notBlank()
+             'author' => V::notBlank(),
+             'location' => V::notBlank(),
          ]);
 
          if ($this->validator->isValid()) {
              $this->mongo->insert([
                  'comment' => $request->getParam('comment'),
-                 'author' => $request->getParam('author')
+                 'author' => $request->getParam('author'),
+                 'location_id' => $request->getParam('location')
              ])->flush('comment');
 
              $this->flash('success', 'Comment added');
          }else{
              $this->flash('danger', 'Something went wrong');
          }
+
          return $this->redirect($response, 'home');
     }
 
