41d9b824d197f8bbaa45c5732fc8086e997254fa
xchopin <xchopin54@gmail.com>
2017-01-05 14:09:58 +0100

    Added comment insertions

--- src/App/Controller/CommentController.php
+++ src/App/Controller/CommentController.php
@@ -6,12 +6,32 @@ use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Respect\Validation\Validator as V;
 
-class CommentController
+class CommentController extends Controller
 {
 
-    public function API_get()
+    public function API_get(Request $request, Response $response, Array $arg)
     {
-        return json_encode($this->mongo->findById('Comment', $id));
+        return json_encode($this->mongo->findById('Comment', $arg['id']));
+    }
+
+    public function add(Request $request, Response $response)
+    {
+         $this->validator->validate($request, [
+             'comment' => V::notBlank(),
+             'author' => V::notBlank()
+         ]);
+
+         if ($this->validator->isValid()) {
+             $this->mongo->insert([
+                 'comment' => $request->getParam('comment'),
+                 'author' => $request->getParam('author')
+             ])->flush('comment');
+
+             $this->flash('success', 'Comment added');
+         }else{
+             $this->flash('danger', 'Something went wrong');
+         }
+         return $this->redirect($response, 'home');
     }
 
 }
