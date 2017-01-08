6ce04ff547fb4c5eb285bc231730ce8ea873c732
xchopin <xchopin54@gmail.com>
2017-01-08 19:53:35 +0100

    Added a new way to test the speed execution when it fails

--- src/App/Controller/AppController.php
+++ src/App/Controller/AppController.php
@@ -9,13 +9,15 @@ class AppController extends Controller
 {
     public function home(Request $request, Response $response)
     {
-        /**
-         *  We thought about you Mr Nourissier ;-)
-         */
+        // We thought about you Mr Nourissier ;-)
         $time1 = new \DateTime();
         $points =  $this->mongo->findAll('point');
         $time2 = new \DateTime();
+
+        // Depending the PHP Version
         $diff = $time1->getTimestamp() - $time2->getTimestamp();
+        if ($diff == 0)
+            $diff = $time1->diff($time2)->f;
 
         return $this->view->render($response, 'App/home.twig', [
             'points' => $points->toArray(),
